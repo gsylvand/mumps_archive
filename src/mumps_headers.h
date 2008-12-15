34ec1caed32a2ec9e2194fda4cb7cf91b76a17a7
The MUMPS Team <contact@mumps-tech.com>
2008-12-15 15:31:38 +0000

    MUMPS v4.8.4

--- src/mumps_headers.h
+++ src/mumps_headers.h
@@ -1,11 +1,10 @@
 C
-C   THIS FILE IS PART OF MUMPS VERSION 4.7.3
-C   This Version was built on Fri May  4 15:54:01 2007
+C  This file is part of MUMPS 4.8.4, built on Mon Dec 15 15:31:38 UTC 2008
 C
 C
 C  This version of MUMPS is provided to you free of charge. It is public
 C  domain, based on public domain software developed during the Esprit IV
-C  European project PARASOL (1996-1999) by CERFACS, ENSEEIHT-IRIT and RAL. 
+C  European project PARASOL (1996-1999) by CERFACS, ENSEEIHT-IRIT and RAL.
 C  Since this first public domain version in 1999, the developments are
 C  supported by the following institutions: CERFACS, ENSEEIHT-IRIT, and
 C  INRIA.
@@ -14,17 +13,17 @@ C  Main contributors are Patrick Amestoy, Iain Duff, Abdou Guermouche,
 C  Jacko Koster, Jean-Yves L'Excellent, and Stephane Pralet.
 C
 C  Up-to-date copies of the MUMPS package can be obtained
-C  from the Web pages http://mumps.enseeiht.fr/
-C  or http://graal.ens-lyon.fr/MUMPS
+C  from the Web pages:
+C  http://mumps.enseeiht.fr/  or  http://graal.ens-lyon.fr/MUMPS
 C
 C
 C   THIS MATERIAL IS PROVIDED AS IS, WITH ABSOLUTELY NO WARRANTY
-C   EXPRESSED OR IMPLIED.  ANY USE IS AT YOUR OWN RISK.
+C   EXPRESSED OR IMPLIED. ANY USE IS AT YOUR OWN RISK.
 C
 C
 C  User documentation of any code that uses this software can
 C  include this complete notice. You can acknowledge (using
-C  references [1], [2], and [3] the contribution of this package
+C  references [1], [2], and [3]) the contribution of this package
 C  in any scientific publication dependent upon the use of the
 C  package. You shall use reasonable endeavours to notify
 C  the authors of the package of this publication.
@@ -42,30 +41,22 @@ C   [3] P. R. Amestoy and A. Guermouche and J.-Y. L'Excellent and
 C   S. Pralet, Hybrid scheduling for the parallel solution of linear
 C   systems. Parallel Computing Vol 32 (2), pp 136-156 (2006).
 C
-C
-C     Common header positions:
-C
-C     XXI    ->  size of integer record
-C     XXR    ->  size of real record
-C     XXS    ->  state of the node
-C     XXN    ->  node number
-C     XXP    ->  pointer to previous record
-C
-C  During Solve phase
-C     XXS --> state of the master of a level 2 node:
-C             -9999  =  undefined
-C             C_FINI (no more contribution need be received)
-C
-      INTEGER XXI, XXR, XXS, XXN, XXP, XSIZE
-      PARAMETER(XXI=0,XXR=1,XXS=2,XXN=3,XXP=4,XSIZE=5)
-C     ------------------------
-C     Compressed CB of level 1
-C     ------------------------
+      INTEGER XXI, XXR, XXS, XXN, XXP
+      PARAMETER(XXI=0,XXR=1,XXS=2,XXN=3,XXP=4)
+      INTEGER XXVADDRL, XXWRITTENL
+      PARAMETER(XXVADDRL=5,XXWRITTENL=7)
+      INTEGER XXNDIAG2W  
+      PARAMETER(XXNDIAG2W=8)
+      INTEGER XXVADDRU, XXWRITTENU
+      PARAMETER(XXVADDRU=9,XXWRITTENU=11)
+      INTEGER XSIZE_IC, XSIZE_OOC_SYM, XSIZE_OOC_UNSYM
+      INTEGER XSIZE_OOC_NOPANEL 
+      PARAMETER (XSIZE_IC=5,XSIZE_OOC_SYM=9,XSIZE_OOC_UNSYM=12,
+     *           XSIZE_OOC_NOPANEL=7)
+      INTEGER IXSZ
+      PARAMETER(IXSZ= 222)    
       INTEGER S_CB1COMP
       PARAMETER (S_CB1COMP=314)
-C     ----------------------
-C     State of type 2 strips
-C     ----------------------
       INTEGER S_ACTIVE, S_ALL, S_NOLCBCONTIG,
      *        S_NOLCBNOCONTIG, S_NOLCLEANED,
      *        S_NOLCBNOCONTIG38, S_NOLCBCONTIG38,
@@ -74,34 +65,13 @@ C     ----------------------
      *          S_NOLCBNOCONTIG=403, S_NOLCLEANED=404,
      *          S_NOLCBNOCONTIG38=405, S_NOLCBCONTIG38=406,
      *          S_NOLCLEANED38=407,C_FINI=1)
-C     (note: S_NOLCBNOCONTIG38 corresponds to nodes
-C            that only have NELIMxNBROW significant
-C            entries and that can be compacted accordingly)
-C
-C     Free : the record can be compressed/removed
       INTEGER S_FREE, S_NOTFREE
       PARAMETER(S_FREE=54321,S_NOTFREE=-123456)
-C
-C
       INTEGER TOP_OF_STACK
       PARAMETER(TOP_OF_STACK=-999999)
-C
-C     -----------------------
-C     XTRA_SLAVES_SYM: extra dummy slaves added
-C     for strips in the symmetric case to hold:
-C       - counter of BLFAC_SLAVE messages received
-C       - NFRONT
-C       - information about recursivity and reception
-C         of ROOT2SON during recursivity
-C         and strip cannot be freed yet.
-      INTEGER XTRA_SLAVES_SYM, S_ROOT2SON_CALLED
-      PARAMETER(XTRA_SLAVES_SYM=3, S_ROOT2SON_CALLED=-341)
-
-C
-C FIXME, later:
-C   New state: S_ONDISK ? Maybe not.
-C   IW(XXSIZEONDISK) could provide size written on disk.
-C                   = 0 => in-core
-C                   = IW(XXR) => all OOC ?
-C         pb with type 2 bands, etc to be discussed.
-C
+      INTEGER XTRA_SLAVES_SYM, XTRA_SLAVES_UNSYM
+      PARAMETER(XTRA_SLAVES_SYM=3, XTRA_SLAVES_UNSYM=1)
+         INTEGER S_ROOT2SON_CALLED, S_REC_CONTSTATIC, 
+     &  S_ROOTBAND_INIT
+         PARAMETER(S_ROOT2SON_CALLED=-341,S_REC_CONTSTATIC=1,
+     &             S_ROOTBAND_INIT=0)
--- src/mumps_tags.h
+++ src/mumps_tags.h
@@ -1,11 +1,10 @@
 C
-C   THIS FILE IS PART OF MUMPS VERSION 4.7.3
-C   This Version was built on Fri May  4 15:54:01 2007
+C  This file is part of MUMPS 4.8.4, built on Mon Dec 15 15:31:38 UTC 2008
 C
 C
 C  This version of MUMPS is provided to you free of charge. It is public
 C  domain, based on public domain software developed during the Esprit IV
-C  European project PARASOL (1996-1999) by CERFACS, ENSEEIHT-IRIT and RAL. 
+C  European project PARASOL (1996-1999) by CERFACS, ENSEEIHT-IRIT and RAL.
 C  Since this first public domain version in 1999, the developments are
 C  supported by the following institutions: CERFACS, ENSEEIHT-IRIT, and
 C  INRIA.
@@ -14,17 +13,17 @@ C  Main contributors are Patrick Amestoy, Iain Duff, Abdou Guermouche,
 C  Jacko Koster, Jean-Yves L'Excellent, and Stephane Pralet.
 C
 C  Up-to-date copies of the MUMPS package can be obtained
-C  from the Web pages http://mumps.enseeiht.fr/
-C  or http://graal.ens-lyon.fr/MUMPS
+C  from the Web pages:
+C  http://mumps.enseeiht.fr/  or  http://graal.ens-lyon.fr/MUMPS
 C
 C
 C   THIS MATERIAL IS PROVIDED AS IS, WITH ABSOLUTELY NO WARRANTY
-C   EXPRESSED OR IMPLIED.  ANY USE IS AT YOUR OWN RISK.
+C   EXPRESSED OR IMPLIED. ANY USE IS AT YOUR OWN RISK.
 C
 C
 C  User documentation of any code that uses this software can
 C  include this complete notice. You can acknowledge (using
-C  references [1], [2], and [3] the contribution of this package
+C  references [1], [2], and [3]) the contribution of this package
 C  in any scientific publication dependent upon the use of the
 C  package. You shall use reasonable endeavours to notify
 C  the authors of the package of this publication.
@@ -42,41 +41,23 @@ C   [3] P. R. Amestoy and A. Guermouche and J.-Y. L'Excellent and
 C   S. Pralet, Hybrid scheduling for the parallel solution of linear
 C   systems. Parallel Computing Vol 32 (2), pp 136-156 (2006).
 C
-C     $Id: mumps_tags.h,v 1.6 2007/03/23 15:39:09 jylexcel Exp $
-C -----------------------------------------
-C  This file contains the definition
-C  of all tags.
-C -----------------------------------------
-C
-C -----------------------------------------
-C  Tag for arrowheads distribution
-C -----------------------------------------
       INTEGER ARROWHEAD, ARR_INT, ARR_REAL, ELT_INT, ELT_REAL
       PARAMETER ( ARROWHEAD = 20,
      *            ARR_INT = 29,
      *            ARR_REAL = 30,
      *            ELT_INT = 31,
      *            ELT_REAL = 32 )
-
-C ----------------------------------------------------
-C   Tags for collecting distributed integer info
-C   for analysis in case of initial distributed matrix
-C ----------------------------------------------------
       INTEGER COLLECT_NZ, COLLECT_IRN, COLLECT_JCN
       PARAMETER( COLLECT_NZ  = 35,
      *           COLLECT_IRN = 36,
      *           COLLECT_JCN = 37 )
-
-C -----------------------------------------
-C   Tags for factorization
-C -----------------------------------------
       INTEGER RACINE,
      *        NOEUD,
      *        TERREUR,
      *        MAITRE_DESC_BANDE,
      *        MAITRE2,
      *        BLOC_FACTO,
-     *        CONTSIPERENIV2,
+     *        CONTRIB_TYPE2,
      *        MAPLIG,
      *        FACTOR
       PARAMETER ( RACINE            = 2,
@@ -84,14 +65,10 @@ C -----------------------------------------
      *            MAITRE_DESC_BANDE = 4,
      *            MAITRE2           = 5,
      *            BLOC_FACTO        = 6,
-     *            CONTSIPERENIV2    = 7,
+     *            CONTRIB_TYPE2     = 7,
      *            MAPLIG            = 8,
      *            FACTOR            = 9,
      *            TERREUR           = 99 )
-
-C -----------------------------------------
-C   Tags for assembly of root (in facto)
-C -----------------------------------------
       INTEGER ROOT_NELIM_INDICES,
      *        ROOT_CONT_STATIC,
      *        ROOT_NON_ELIM_CB,
@@ -102,10 +79,6 @@ C -----------------------------------------
      *        ROOT_NON_ELIM_CB       = 17,
      *        ROOT_2SLAVE            = 18,
      *        ROOT_2SON              = 19 )
-
-C -----------------------------------------
-C   Tags for solve
-C -----------------------------------------
       INTEGER RACINE_SOLVE,
      *        ContVec,
      *        Master2Slave,
@@ -118,46 +91,26 @@ C -----------------------------------------
      *           GatherSol    = 13,
      *           ScatterRhsI  = 54,
      *           ScatterRhsR  = 55)
-
-C -----------------------------------------
-C   Tags for backsolve
-C -----------------------------------------
       INTEGER FEUILLE,
      *        BACKSLV_UPDATERHS,
      *        BACKSLV_MASTER2SLAVE
       PARAMETER( FEUILLE = 21,
      *           BACKSLV_UPDATERHS = 22,
      *           BACKSLV_MASTER2SLAVE = 23 )
-C ------------------------
-C   Tag for symmetrization
-C ------------------------
       INTEGER SYMMETRIZE
       PARAMETER ( SYMMETRIZE = 24 )
-
-C ----------------------------
-C   Tags specific to symmetric
-C ----------------------------
       INTEGER BLOC_FACTO_SYM,
      *        BLOC_FACTO_SYM_SLAVE, END_NIV2_LDLT
-
       PARAMETER ( BLOC_FACTO_SYM = 25,
      *            BLOC_FACTO_SYM_SLAVE = 26 , 
      *            END_NIV2_LDLT = 33 )
-C -------------------------------------
-C   Tags specific to dynamic scheduling
-C -------------------------------------
       INTEGER UPDATE_LOAD
       PARAMETER ( UPDATE_LOAD = 27 )
-
-C   To send deficientcy
       INTEGER DEFIC_TAG
       PARAMETER(  DEFIC_TAG = 28 )
-C   To send Schur
       INTEGER TAG_SCHUR
       PARAMETER( TAG_SCHUR = 38 )
-C   To clean up IRECV
       INTEGER TAG_DUMMY
       PARAMETER( TAG_DUMMY = 39 )
-C   To send zero pivot indices
       INTEGER ZERO_PIV
       PARAMETER( ZERO_PIV = 40 )
